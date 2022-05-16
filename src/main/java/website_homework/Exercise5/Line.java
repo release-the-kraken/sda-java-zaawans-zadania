9a23b8c6a305b75e7918ea5a209129eed472f03a
release_the_kraken <wmichal13@wp.pl>
2022-05-16 21:17:18 +0200

    initial commit

---
+++ src/main/java/website_homework/Exercise5/Line.java
@@ -0,0 +1,41 @@
+package website_homework.Exercise5;
+
+import website_homework.Exercise1.Point2D;
+
+public class Line {
+    Point2D lineBeginPoint;
+    Point2D lineEndPoint;
+
+    private Line(Point2D lineBeginPoint, Point2D lineEndPoint) {
+        this.lineBeginPoint = new Point2D();
+        this.lineEndPoint = new Point2D();
+    }
+
+    private Line(float beginPointX, float beginPointY, float endPointX, float endPointY) {
+        this.lineBeginPoint = new Point2D(beginPointX, beginPointY);
+        this.lineEndPoint = new Point2D(endPointX, endPointY);
+    }
+    static Line from(Point2D lineBeginPoint, Point2D lineEndPoint){
+        return new Line(lineBeginPoint, lineEndPoint);
+    }
+    static Line of(float beginPointX, float beginPointY, float endPointX, float endPointY){
+        return new Line(beginPointX, beginPointY, endPointX, endPointY);
+    }
+    float getLength(){
+        float differenceOfXs = lineBeginPoint.getX() - lineEndPoint.getX();
+        float differenceOfYs = lineBeginPoint.getY() - lineEndPoint.getY();
+        float powerOfDiffX = (float) Math.pow(differenceOfXs, 2);
+        float powerOfDiffY = (float) Math.pow(differenceOfYs, 2);
+        float result = (float) Math.sqrt(powerOfDiffX + powerOfDiffY);
+        return result;
+    }
+    float[] getMidPointCoordinates(){
+        float sumOfXs = lineBeginPoint.getX() + lineEndPoint.getX();
+        float sumOfYs = lineBeginPoint.getY() + lineEndPoint.getY();
+        float middlePointX = sumOfXs / 2;
+        float middlePointY = sumOfYs / 2;
+        float[] middlePointCoordinates = {middlePointX, middlePointY};
+        return middlePointCoordinates;
+    }
+
+}
---
+++ src/main/java/website_homework/Exercise5/LineMain.java
@@ -0,0 +1,18 @@
+package website_homework.Exercise5;
+
+import java.util.Arrays;
+
+public class LineMain {
+    public static void main(String[] args) {
+        runApp();        
+    }
+
+    private static void runApp() {
+        Line line = Line.of(3, 4, 9, 12);
+        System.out.println("Starting point = [" + line.lineBeginPoint.getX() + ", " + line.lineBeginPoint.getY() + "]");
+        System.out.println("Ending point = [" + line.lineEndPoint.getX() + ", " + line.lineEndPoint.getY() + "]");
+        System.out.println("Line length = " + line.getLength());
+        System.out.println("Mid point coordinates = " + Arrays.toString(line.getMidPointCoordinates()));
+
+    }
+}
