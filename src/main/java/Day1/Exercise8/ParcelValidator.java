6d3e8f06dbb4a08ff082d7e78072f533193a3da0
release_the_kraken <wmichal13@wp.pl>
2022-05-22 16:01:55 +0200

    initial commit

--- src/main/java/Day1/Exercise8/ParcelValidator.java
+++ src/main/java/Day1/Exercise8/ParcelValidator.java
@@ -15,7 +15,7 @@ public class ParcelValidator implements Validator{
     }
 
     boolean isDimensionInLimit(Parcel parcel) {
-        if (parcel.zLength < 30 && parcel.xLength < 30 && parcel.yLength < 30) {
+        if (parcel.zLength < 30 || parcel.xLength < 30 || parcel.yLength < 30) {
             System.out.println("Dimension less than 30.");
             return false;
         }
@@ -27,10 +27,10 @@ public class ParcelValidator implements Validator{
             System.out.println("Weight limit for non express package exceeded.");
             return false;
         }
-//        if(parcel.isExpress && parcel.weight > 15 ) {
-//            System.out.println("Weigh limit for express package exceeded.");
-//            return false;
-//        }
+        if(parcel.isExpress && parcel.weight > 15 ) {
+            System.out.println("Weigh limit for express package exceeded.");
+            return false;
+        }
         return true;
     }
 }
