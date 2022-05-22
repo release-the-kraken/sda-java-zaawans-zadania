f245541e114e788a378c417f3699fda42202130b
release_the_kraken <wmichal13@wp.pl>
2022-05-22 15:59:45 +0200

    initial commit

---
+++ src/main/java/Day1/Exercise8/Parcel.java
@@ -0,0 +1,16 @@
+package Day1.Exercise8;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class Parcel{
+    int xLength;
+    int yLength;
+    int zLength;
+    float weight;
+    boolean isExpress;
+
+
+}
---
+++ src/main/java/Day1/Exercise8/Validator.java
@@ -0,0 +1,5 @@
+package Day1.Exercise8;
+
+public interface Validator {
+    boolean validate(Parcel input);
+}
